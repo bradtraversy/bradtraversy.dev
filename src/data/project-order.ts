ddf94779353788bff852ef0bded0adcd7a7aa73d
bradtraversy <traversymedia@gmail.com>
2026-07-20 06:24:03 -0400

    content: pin devsheets project order

---
+++ src/data/project-order.ts
@@ -0,0 +1,29 @@
+type ProjectLike = {
+  id: string;
+  data: {
+    publishDate: Date;
+  };
+};
+
+export const homepageProjectSlugs = [
+  'devsheets',
+  'skillpass',
+  'portdoc',
+  'vidpipe',
+  'multiprompter',
+];
+
+const pinnedProjectSlugs = ['devsheets'];
+
+const projectRank = (id: string) => {
+  const index = pinnedProjectSlugs.indexOf(id);
+  return index === -1 ? pinnedProjectSlugs.length : index;
+};
+
+export const sortProjectsForDisplay = <T extends ProjectLike>(projects: T[]) =>
+  [...projects].sort((a, b) => {
+    const rankDiff = projectRank(a.id) - projectRank(b.id);
+    if (rankDiff !== 0) return rankDiff;
+
+    return b.data.publishDate.valueOf() - a.data.publishDate.valueOf();
+  });
